d4868cf3714455b511380f2b858922efcf1e5ab5
18537591339 <361268915@qq.com>
2018-12-29 21:15:27 +0800

    first commit

---
+++ src/main/java/com/git/boetar/weinispringboot/WeiniSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.git.boetar.weinispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiniSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeiniSpringbootApplication.class, args);
+    }
+
+}
+
